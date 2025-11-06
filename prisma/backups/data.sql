2502925e9dee3b982b791c279021c24c4fd19266
louiselaine599-wq <louiselaine599-wq@users.noreply.github.com>
2025-11-06 00:44:19 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict Dd2wz0jp4kUyh35n0ohVRG12X7uCqzdfoiQAB7kkeggGmsqGlICtQDc5xvkxUDV
+-- \restrict l7aa9R4O4ehMQmRVD5x5dOatYFqelJjNTN1yj0VbToqLFBJZ0b1UqdfX28yzVee
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -73,7 +73,7 @@ COPY "auth"."oauth_clients" ("id", "client_secret_hash", "registration_type", "r
 -- Data for Name: sessions; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."sessions" ("id", "user_id", "created_at", "updated_at", "factor_id", "aal", "not_after", "refreshed_at", "user_agent", "ip", "tag", "oauth_client_id") FROM stdin;
+COPY "auth"."sessions" ("id", "user_id", "created_at", "updated_at", "factor_id", "aal", "not_after", "refreshed_at", "user_agent", "ip", "tag", "oauth_client_id", "refresh_token_hmac_key", "refresh_token_counter") FROM stdin;
 \.
 
 
@@ -89,7 +89,7 @@ COPY "auth"."mfa_amr_claims" ("session_id", "created_at", "updated_at", "authent
 -- Data for Name: mfa_factors; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."mfa_factors" ("id", "user_id", "friendly_name", "factor_type", "status", "created_at", "updated_at", "secret", "phone", "last_challenged_at", "web_authn_credential", "web_authn_aaguid") FROM stdin;
+COPY "auth"."mfa_factors" ("id", "user_id", "friendly_name", "factor_type", "status", "created_at", "updated_at", "secret", "phone", "last_challenged_at", "web_authn_credential", "web_authn_aaguid", "last_webauthn_challenge_data") FROM stdin;
 \.
 
 
@@ -208,6 +208,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict Dd2wz0jp4kUyh35n0ohVRG12X7uCqzdfoiQAB7kkeggGmsqGlICtQDc5xvkxUDV
+-- \unrestrict l7aa9R4O4ehMQmRVD5x5dOatYFqelJjNTN1yj0VbToqLFBJZ0b1UqdfX28yzVee
 
 RESET ALL;
